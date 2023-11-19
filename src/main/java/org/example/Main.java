ded8327ee94015d04f98dc7869e562d5042b8ed8
МVi-esina <viktorija.esman@gmail.com>
2023-11-20 00:15:42 +0300

    hw1_MyServiceJDBC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,17 @@ package org.example;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.example.controller.CustomerController;
+import org.example.repository.CustomerRepository;
 import servlet.CustomerServlet;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        CustomerController customerController=new CustomerController();
+        customerController.showAllPerson();
 
-
-        Server server = new Server(8080); // Порт может быть изменен на ваше усмотрение
+        Server server = new Server(8080);
 
         ServletContextHandler servletContextHandler = new ServletContextHandler();
         servletContextHandler.setContextPath("/");
